d1b7e17fb87aec2b61cc19d3aa2a265d7d791f47
MatiasLA1 <mlaguilar@fi.uba.ar>
2025-09-29 00:09:28 -0300

    Se agregaron los pdfs. Y se termino todo el diseno del html y css para los templates del index, registration y base.html. Todos fueron adaptados para distintas resoluciones de pantalla. Falta trabajar con la programacion de envio de formulario.

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 from flask import Flask, render_template, url_for
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
 info_evento = {
     1: { "nombre": "Rally MTB 2025",
     "organizador": "Club Social y Deportivo Unidos por el Deporte",
-    "descripcion": "Carrera de MTB rural en dos modalidades 30km y 80km …",
-    "fecha": "24 de Octubre de 2025",
+    "descripcion": "Carrera de MTB rural en dos modalidades 30km y 80km. Viví la experiencia del mountain bike en uno de los paisajes más desafiantes de la Argentina. Una jornada única de deporte, naturaleza y adrenalina.",
+    "fecha": "16 de noviembre de 2025",
     "horario": "8am",
     "lugar": "Tandil, Buenos Aires",
     "tipo_carrera": "MTB rural",
     "modalidad_costo": {
     1: {"nombre": "Corta" ,"valor": "100"},
     2: {"nombre": "Larga" ,"valor": "200"}},
-    "Auspiciantes": ["ausp1","auspN"]
+    "Auspiciantes": ["Powerade","Specialized"]
     }
 }
 
 @app.route('/')
 def index():
-    return render_template('index.html',organizador=info_evento[1]["organizador"],nombre_evento=info_evento[1]["nombre"])
+    return render_template('index.html',
+                           nombre=info_evento[1]["nombre"],
+                           organizador=info_evento[1]["organizador"],
+                           nombre_evento=info_evento[1]["nombre"],
+                           descripcion_evento=info_evento[1]["descripcion"],
+                           tipo_carrera=info_evento[1]["tipo_carrera"],
+                           fecha=info_evento[1]["fecha"],
+                           lugar=info_evento[1]["lugar"],
+                           )
 
 @app.route('/base')
 def base():
